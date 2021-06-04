3d907a1835163ea0abfdf0e2d273b858c1d29a92
Jon Shipley <js@ovix.co.uk>
2021-06-04 14:16:41 +0100

    [db, tslib] update ps report to store 1024 chars for keystrokes stored

---
+++ db/migrations/schema-objects/20210604135432.do.mtc-results-ps-report-alter-keystoke-length.sql
@@ -0,0 +1,74 @@
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q1K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q2K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q3K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q4K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q5K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q6K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q7K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q8K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q9K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q10K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q11K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q12K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q13K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q14K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q15K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q16K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q17K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q18K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q19K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q20K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q21K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q22K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q23K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q24K NVARCHAR(1024);
+
+ALTER TABLE mtc_results.psychometricReport
+ALTER COLUMN Q25K NVARCHAR(1024);
